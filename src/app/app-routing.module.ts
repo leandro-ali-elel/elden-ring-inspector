c64f9f67c5d95479e54a52f0557cfecd316c1f24
lali <lali@riu.com>
2023-03-12 19:21:24 -0300

    created path and routes to scripts, items and home. created common tag for multi language

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AppComponent,
     children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./views/home/home.module').then((m) => m.HomeModule),
+      },
       {
         path: 'items',
         loadChildren: () =>
           import('./views/items/items.module').then((m) => m.ItemsModule),
+      },
+      {
+        path: 'scripts',
+        loadChildren: () =>
+          import('./views/scripts/scripts.module').then((m) => m.ScriptsModule),
+      },
+      {
+        path: '**',
+        redirectTo: '',
+        pathMatch: 'full',
       },
     ],
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/views/layout/header/header.module.ts
+++ src/app/views/layout/header/header.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import { TranslocoRootModule } from 'src/app/core/modules/transloco-root.module';
 import { HeaderComponent } from './header.component';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, TranslocoRootModule, RouterModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
