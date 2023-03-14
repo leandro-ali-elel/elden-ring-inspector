ebbe3fb3f1a3bb8ba5d957648ae613c6dff97fa9
lali <lali@riu.com>
2023-03-14 17:53:29 -0300

    added ashes routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,11 @@ const routes: Routes = [
           import('./views/home/home.module').then((m) => m.HomeModule),
       },
       {
+        path: 'ashes',
+        loadChildren: () =>
+          import('./views/ashes/ashes.module').then((m) => m.AshesModule),
+      },
+      {
         path: 'items',
         loadChildren: () =>
           import('./views/items/items.module').then((m) => m.ItemsModule),
