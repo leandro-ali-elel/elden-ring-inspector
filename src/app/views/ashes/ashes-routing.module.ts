975c22268715fe944f02b6bfcf0abf0796a3b740
lali <lali@riu.com>
2023-03-14 17:54:30 -0300

    added css to ashes

--- src/app/views/ashes/ashes-routing.module.ts
+++ src/app/views/ashes/ashes-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AshesComponent } from './ashes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AshesComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AshesRoutingModule { }
+export class AshesRoutingModule {}
